37f4b2b2de0dc4767bc4f18bf7830083e923df41
nolifer_jpg <roysouhard2706@gmail.com>
2025-05-23 20:33:00 +0530

    Add solution: 59A.py

---
+++ 800-py/59A.py
@@ -0,0 +1,16 @@
+s = input()
+upper = 0
+lower = 0
+
+for char in s:
+    if char.isupper():
+        upper+=1
+    else:
+        lower+=1
+
+if upper == lower:
+    print(s.lower())
+elif upper>lower:
+    print(s.upper())
+else:
+    print(s.lower())
