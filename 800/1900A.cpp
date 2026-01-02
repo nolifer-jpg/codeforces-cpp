38f73f24018358906d7b09c47d7ae75a6aedc8a8
nolifer-jpg <roysouhard2706@gmail.com>
2026-01-03 03:22:51 +0530

    1900A.cpp

---
+++ 800/1900A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vi vector<int>
+#define fori(i,a,b) for(int i=a;i<b;i++)
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+
+void solve() {
+  int n; cin >>n;
+}
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t; cin >> t;
+  while(t--)
+  {
+     solve();
+  }
+  return 0;
+}
